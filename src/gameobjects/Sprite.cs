b21603452e4d3265088265ea5424ffbe40691342
voiddev <brotales26@gmail.com>
2024-07-21 22:04:33 -0400

    source folder :]

--- src/gameobjects/Sprite.cs
+++ src/gameobjects/Sprite.cs
@@ -1,13 +1,15 @@
 using System.Numerics;
 using Raylib_cs;
-public class Sprite{
+public class Sprite : GameObject{
     public Vector2 position = Vector2.Zero;
     public Vector2 scale = Vector2.One;
     public float rotation = 0.0f;
     public Texture2D texture;
-    public void draw(){
+    public Sprite(){
+        
+    }
+    public override void Draw(){
         if(!texture.Equals(null))
             Raylib.DrawTexturePro(texture,new Rectangle(0,0,texture.Width,texture.Height),new Rectangle(position,texture.Width,texture.Height),new Vector2(texture.Width*0.5f,texture.Height*0.5f),rotation,Color.White);
     }
-    public void update(float dt){}
 }
